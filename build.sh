c850d1e776a92a500886b52bfb0a98006dfb5ff3
Utkarsh Ahuja <ahujautkarsh80@gmail.com>
2024-03-15 22:41:04 +0530

    add expat

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-mkdir build
 cd build
 cmake -G "MinGW Makefiles" ..
 cmake --build .
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <expat/lib/expat.h>
 
 int main()
 {
+    XML_Parser parser = XML_ParserCreate(NULL);
     std::cout << "Template\n";
 }
